709ff2c69f1f7ea3fa5659957c8c88d23de10ae0
fedorovse <ingor-ru@mail.ru>
2020-05-19 13:21:11 +0700

    5.1.4. Создать convert(Iterator<Iterator>) [#286013]

--- chapter_001/src/main/java/ru/job4j/it/Converter.java
+++ chapter_001/src/main/java/ru/job4j/it/Converter.java
@@ -8,29 +8,31 @@ public class Converter {
     public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
 
         return new Iterator<Integer>() {
-            private Iterator<Integer> iterator = it.next();
+            private Iterator<Integer> iterator = initIt();
 
-            private void findIt() {
-//                Iterator<Integer> rsl;
+            private Iterator<Integer> initIt() {
+                iterator = it.next();
+                return findIt();
+            }
 
+            private Iterator<Integer> findIt() {
+                Iterator<Integer> rsl = iterator;
                 while (it.hasNext()) {
-                    if (!iterator.hasNext()) {
-                        iterator.next();
-                    } else {
+                    if (rsl.hasNext()) {
                         break;
+                    } else {
+                        rsl = it.next();
                     }
                 }
-//                return rsl;
+                return rsl;
             }
 
             @Override
             public boolean hasNext() {
-                boolean rsl = false;
                 if (!iterator.hasNext()) {
-                    findIt();
-                    rsl = iterator.hasNext();
+                    iterator = findIt();
                 }
-                return rsl;
+                return iterator.hasNext();
             }
 
             @Override
@@ -38,11 +40,8 @@ public class Converter {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-
-                return it.next().next();
+                return findIt().next();
             }
-
-//            https://ru.stackoverflow.com/questions/612458/%D0%9E%D0%B1%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%B0-%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2
         };
     }
 }
