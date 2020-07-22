fc3050b5d9e46c1702ecde5343eacbd2910f6fad
fedorovse <ingor-ru@mail.ru>
2020-07-22 16:38:11 +0700

    8. Реализовать собственную структуру данных - HashMap[#286035]_work

--- chapter_001/src/test/java/ru/job4j/collection/UserMapTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/UserMapTest.java
@@ -1,14 +1,16 @@
 package ru.job4j.collection;
 
-import org.w3c.dom.ls.LSOutput;
+
+import org.junit.Test;
 
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapTest {
+public class UserMapTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void whenPutTwoEqualUsersThenPrintOne() {
         User user1 = new User("Sergey", 2, new GregorianCalendar(2020, 07, 17));
         User user2 = new User("Sergey", 2, new GregorianCalendar(2020, 07, 17));
         Map<User, Object> users = new HashMap<>();
