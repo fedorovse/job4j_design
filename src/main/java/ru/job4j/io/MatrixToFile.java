974135162adfab24905cd6f198d2106e0ced72e1
fedorovse <ingor-ru@mail.ru>
2022-08-12 17:08:03 +0700

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MatrixToFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MatrixToFile {
+
+    public static int[][] multiple(int size) {
+        int[][] result = new int[size][size];
+        for (int row = 0; row < result.length; row++) {
+            for (int cell = 0; cell < result[row].length; cell++) {
+                result[row][cell] = (row + 1) * (cell + 1);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] rsl = multiple(9);
+        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
+            for (int[] row : rsl) {
+                for (int cell : row) {
+                    out.write((cell + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
