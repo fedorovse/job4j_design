22e21e270223c8d21c3c0e44b7e5a5afea4253a7
fedorovse <ingor-ru@mail.ru>
2022-08-19 07:51:49 +0700

    2. Анализ доступности сервера. [#859 #333716]_2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,23 +6,20 @@ public class Analizy {
 
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileWriter(target))) {
+             PrintWriter out = new PrintWriter(new BufferedWriter(new PrintWriter(target)))) {
             String logStr;
-            String notConnected;
-            String connected;
+            String notConnected = null;
+            String connected = "";
             while ((logStr = in.readLine()) != null) {
-                if (logStr.startsWith("400") || logStr.startsWith("500")) {
+                if (notConnected == null
+                        && (logStr.startsWith("400") || logStr.startsWith("500"))) {
                     notConnected = logStr.substring(logStr.indexOf(" ") + 1);
-                    while (logStr.startsWith("400") || logStr.startsWith("500")) {
-                        logStr = in.readLine();
-                        if (logStr == null) {
-                            throw new IllegalArgumentException(
-                                    String.format("%s contains only disconnect time", source)
-                            );
-                        }
-                    }
+                }
+                if (notConnected != null
+                        && (logStr.startsWith("200") || logStr.startsWith("300"))) {
                     connected = logStr.substring(logStr.indexOf(" ") + 1);
                     out.printf("%s;%s;%s", notConnected, connected, System.lineSeparator());
+                    notConnected = null;
                 }
             }
         } catch (IOException e) {
