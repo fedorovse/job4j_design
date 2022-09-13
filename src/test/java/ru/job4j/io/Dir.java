2420ecfab72f0b3bd8b9afd8526f327424ab2f3b
fedorovse <ingor-ru@mail.ru>
2022-09-13 19:26:47 +0700

    4.0. File [#252491]

---
+++ src/test/java/ru/job4j/io/Dir.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Dir {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subFile : Objects.requireNonNull(file.listFiles())) {
+            if (!subFile.isDirectory()) {
+                System.out.printf("File name: %s has size: %s%n",
+                        subFile.getName(),
+                        subFile.length());
+            }
+        }
+    }
+}
