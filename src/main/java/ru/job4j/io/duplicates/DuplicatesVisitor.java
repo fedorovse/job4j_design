ad56a48995d5089edd84a0c35dca855dd33de506
fedorovse <ingor-ru@mail.ru>
2022-09-20 11:59:06 +0700

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,70 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Поиск и сохранение всех файлов
+ * Поиск дубликатов файлов
+ */
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    /**
+     * filesMap хранит все файлы и пути к ним
+     */
+    private final Map<FileProperty, List<Path>> filesMap = new HashMap<>();
+
+    /**
+     * Поиск всех файлов в заданном каталоге и запись их в Map
+     * @param file Path путь к каталогу где осуществляется поиск
+     * @param attrs BasicFileAttributes
+     * @return FileVisitResult
+     * @throws IOException
+     */
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+        if (!filesMap.containsKey(fileProperty)) {
+            filesMap.put(fileProperty, new ArrayList<>());
+        }
+        filesMap.get(fileProperty).add(file.toAbsolutePath());
+        return super.visitFile(file, attrs);
+    }
+
+    /**
+     * Оставляет в Мар только файлы у которых есть дубликаты
+     * @return Map<FileProperty, List<Path>> который содержит только файлы
+     * у которых есть дубликаты и пути к ним
+     */
+    public Map<FileProperty, List<Path>> getDuplicatesMap() {
+        Map<FileProperty, List<Path>> duplicatesMap = new HashMap<>();
+        for (Map.Entry<FileProperty, List<Path>> entry : filesMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                duplicatesMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return duplicatesMap;
+    }
+
+    /**
+     * Вывод в консоль дубликотов файлов и путей к ним в формате:
+     * Имя файла - размер
+     * 1.путь
+     * 2.путь
+     * и т.д.
+     */
+    public void printDuplicates() {
+        Map<FileProperty, List<Path>> duplicatesMap = getDuplicatesMap();
+        for (Map.Entry<FileProperty, List<Path>> entry : duplicatesMap.entrySet()) {
+            System.out.println(entry.getKey());
+            entry.getValue().forEach(System.out::println);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/duplicates/FileProperty.java
@@ -0,0 +1,61 @@
+package ru.job4j.io.duplicates;
+
+import java.util.Objects;
+
+/**
+ * Контейнер, который хранит имя и размер файла
+ */
+public class FileProperty {
+    /**
+     * size - размер файла
+     */
+    private long size;
+
+    /**
+     * name - имя файла
+     */
+    private String name;
+
+    public FileProperty(long size, String name) {
+        this.size = size;
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FileProperty that = (FileProperty) o;
+        return size == that.size && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + size;
+    }
+}
