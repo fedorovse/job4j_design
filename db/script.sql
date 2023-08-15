0088c24aba2ba993af0fc5acefaafd708ca78411
fedorovse <ingor-ru@mail.ru>
2023-08-15 19:50:02 +0700

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE friend (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100),
+birthday DATE,
+hobbi TEXT,
+nick VARCHAR(30),
+weight INT
+);
+
+INSERT INTO
+friend (name, birthday, hobbi, nick, weight)
+VALUES ('Anton', '1980-12-20', 'football', 'Gonzo', 90);
+
+UPDATE friend SET weight = 85;
+
+DELETE FROM friend;
