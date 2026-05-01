fef5e313e473a337a0900e318f8058eb9d65c3ab
fedorovse <ingor-ru@mail.ru>
2026-05-01 21:55:57 +0700

    3.2. Откат изменений [#504830]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -8,5 +8,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello man!");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello!!! I am not alone!");
+        }
     }
 }
